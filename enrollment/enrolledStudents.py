bc1fe453bc40041d0687580ab35f96baf4e500d0
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2023-03-29 08:45:12 +0100

    Testing module setup for API development

---
+++ enrollment/enrolledStudents.py
@@ -0,0 +1,4 @@
+studentName = "Ezinne Okor"
+studentId = "2245678"
+studentEnrollmentYear = " September 2023"
+print(f"THE STUDENT ENROLLMENT DETAILS IS " + studentName + " " +  studentId + studentEnrollmentYear)
